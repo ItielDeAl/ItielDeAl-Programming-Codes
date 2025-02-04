b10ccc67cddc333d19bce75f7a82d9ce02b8c793
Itiel Alvarez <alfred.delgadillo@ulv.com.mx>
2025-02-04 08:36:08 -0600

    ejercicios

--- second semester/matte graphics/graphich_dery.py
+++ second semester/matte graphics/graphich_dery.py
@@ -1,21 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Derivada
 x = np.arange(0,10,0.5)
-y = (x*x)-(8*x)+9
-
+y = 3/x
+#Recta tangente.
 x1 = np.arange(0,10,0.5)
-y1 = -2*x
+y1 = (-1/3)*x+2
 
 #Interseccion
 xi = 3
 yi = 1
 
+#Imprimir
+plt.plot(x,y, label='Derivada')
+plt.plot(x1,y1,'r--', label='Tangente')
 
-plt.plot(x1,y1)
-plt.plot(x,y)
+#Mostrar texto
+plt.xlabel('eje x')
+plt.ylabel('eje y')
+plt.title('Ejercicio 5.3. A')
 
-plt.xlabel('Lado x')
-plt.ylabel('Lado y')
-plt.title('Ejercicio 5.3. B')
+#Imprimir cuadriculas
+plt.grid(True)
+plt.legend()
+
+#imprimir grafica
 plt.show()
